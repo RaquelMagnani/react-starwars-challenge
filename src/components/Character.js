750e7046ab0d1ac337da80f47f20c856364ae42a
Raquel Magnani <quel.magnani88@gmail.com>
2022-04-28 23:50:43 +0200

    refactor grid layout

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import { Col, ListGroup, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 
 const Character = () => {
@@ -17,26 +17,42 @@ const Character = () => {
   }, []);
   return (
     <div>
-      <Row>
-        <Col>
-          <h1>{`${characterInfo.name}`}</h1>
-          <ListGroup variant="flush">
-            <ListGroup.Item>{`Height: ${characterInfo.height}`}</ListGroup.Item>
-            <ListGroup.Item>{`Weight: ${characterInfo.mass}`}</ListGroup.Item>
-            <ListGroup.Item>{`Hair Color: ${characterInfo.hair_color}`}</ListGroup.Item>
-            <ListGroup.Item>{`Skin Color: ${characterInfo.skin_color}`}</ListGroup.Item>
-            <ListGroup.Item>{`Eye Color: ${characterInfo.eye_color}`}</ListGroup.Item>
-            <ListGroup.Item>{`Birth Year: ${characterInfo.birth_year}`}</ListGroup.Item>
-            <ListGroup.Item>{`Gender: ${characterInfo.gender}`}</ListGroup.Item>
-          </ListGroup>
-        </Col>
-        <Col sm={8}>
-          <img
-            alt={`poster of ${characterInfo.title}`}
-            src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}
-          />
-        </Col>
-      </Row>
+      <Container>
+        <Row>
+          <Col className="container-infos">
+            <h1>{`${characterInfo.name}`}</h1>
+            <ul>
+              <li>
+                <span>Height: </span> {characterInfo.height}
+              </li>
+              <li>
+                <span>Weight: </span> {characterInfo.mass}
+              </li>
+              <li>
+                <span>Hair Color: </span> {characterInfo.hair_color}
+              </li>
+              <li>
+                <span>Skin Color: </span> {characterInfo.skin_color}
+              </li>
+              <li>
+                <span>Eye Color: </span> {characterInfo.eye_color}
+              </li>
+              <li>
+                <span>Birth Year: </span> {characterInfo.birth_year}
+              </li>
+              <li>
+                <span>Gender: </span> {characterInfo.gender}
+              </li>
+            </ul>
+          </Col>
+          <Col sm={8}>
+            <img
+              alt={`poster of ${characterInfo.title}`}
+              src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}
+            />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
--- src/components/Film.js
+++ src/components/Film.js
@@ -1,39 +1,53 @@
 import React, { useEffect, useState, useCallback } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import CharactersList from "./CharacterList";
 
 const Film = () => {
   const [film, setFilm] = useState({});
   const { id } = useParams();
-  console.log(id);
   useEffect(() => {
     fetch(`https://swapi.dev/api/films/${id}/`)
       .then((response) => response.json())
       .then((data) => setFilm(data));
   }, []);
   return (
-    <>
-      <header>
-        <img
-          alt={`poster of ${film.title}`}
-          src={`https://starwars-visualguide.com/assets/img/films/${id}.jpg`}
-        />
-      </header>
-      <main>
-        <div>
-          <h1>{film.title}</h1>
-          <h2>{`Premiere Year: ${new Date(
-            film.release_date
-          ).getFullYear()}`}</h2>
-          <p>{`Director: ${film.director}`}</p>
-          <p>{`About: ${film.opening_crawl}`}</p>
-        </div>
-        <div>
-          <p>Characters: </p>
-          <CharactersList cast={film.characters} />
-        </div>
-      </main>
-    </>
+    <Container fluid className="">
+      <Row>
+        <Col md={8}>
+          <div className="container-infos ">
+            <h1>{film.title}</h1>
+            <p>
+              <span>Premiere Year: </span>
+              {new Date(film.release_date).getFullYear()}
+            </p>
+            <p>
+              {" "}
+              <span>Director: </span>
+              {film.director}
+            </p>
+            <p>
+              <span>About: </span>
+              {film.opening_crawl}
+            </p>
+          </div>
+          <Row>
+            <div className="container-infos">
+              <span>Characters: </span>
+
+              <CharactersList cast={film.characters} />
+            </div>
+          </Row>
+        </Col>
+        <Col md={3}>
+          <img
+            alt={`poster of ${film.title}`}
+            src={`https://starwars-visualguide.com/assets/img/films/${id}.jpg`}
+          />
+        </Col>
+      </Row>
+      <Row></Row>
+    </Container>
   );
 };
 
