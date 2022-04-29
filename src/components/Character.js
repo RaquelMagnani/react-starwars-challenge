43272be55451ccfd09b089ba600d463bc85c4d7f
Raquel Magnani <quel.magnani88@gmail.com>
2022-04-29 18:19:35 +0200

    add home world info to character

--- src/components/Character.js
+++ src/components/Character.js
@@ -6,54 +6,67 @@ import { useParams } from "react-router-dom";
 const Character = () => {
   const { id } = useParams();
   const [characterInfo, setCharacterInfo] = useState({});
+  const [homeWorld, setHomeWorld] = useState({});
   const getData = async () => {
     const response = await fetch(`https://swapi.dev/api/people/${id}/`);
     const data = await response.json();
     setCharacterInfo(data);
   };
+  const getHomeWorldData = async () => {
+    if (characterInfo !== {}) {
+      const response = await fetch(characterInfo.homeworld);
+      const data = await response.json();
+      setHomeWorld(data);
+    }
+  };
 
   useEffect(() => {
     getData();
   }, []);
+  useEffect(() => {
+    getHomeWorldData();
+  }, [characterInfo]);
   return (
-    <div>
-      <Container>
-        <Row>
-          <Col className="container-infos">
-            <h1>{`${characterInfo.name}`}</h1>
-            <ul>
-              <li>
-                <span>Height: </span> {characterInfo.height}
-              </li>
-              <li>
-                <span>Weight: </span> {characterInfo.mass}
-              </li>
-              <li>
-                <span>Hair Color: </span> {characterInfo.hair_color}
-              </li>
-              <li>
-                <span>Skin Color: </span> {characterInfo.skin_color}
-              </li>
-              <li>
-                <span>Eye Color: </span> {characterInfo.eye_color}
-              </li>
-              <li>
-                <span>Birth Year: </span> {characterInfo.birth_year}
-              </li>
-              <li>
-                <span>Gender: </span> {characterInfo.gender}
-              </li>
-            </ul>
-          </Col>
-          <Col sm={8}>
-            <img
-              alt={`poster of ${characterInfo.title}`}
-              src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}
-            />
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <Container>
+      <Row>
+        <Col md={6} className="container-infos">
+          <h1>{`${characterInfo.name}`}</h1>
+          <ul>
+            <li>
+              <span>Height: </span> {characterInfo.height}
+            </li>
+            <li>
+              <span>Weight: </span> {characterInfo.mass}
+            </li>
+            <li>
+              <span>Hair Color: </span> {characterInfo.hair_color}
+            </li>
+            <li>
+              <span>Skin Color: </span> {characterInfo.skin_color}
+            </li>
+            <li>
+              <span>Eye Color: </span> {characterInfo.eye_color}
+            </li>
+            <li>
+              <span>Birth Year: </span> {characterInfo.birth_year}
+            </li>
+            <li>
+              <span>Gender: </span> {characterInfo.gender}
+            </li>
+            <li>
+              <span>Home World: </span> {homeWorld.name}
+            </li>
+          </ul>
+        </Col>
+
+        <Col md={6}>
+          <img
+            alt={`poster of ${characterInfo.title}`}
+            src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}
+          />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
