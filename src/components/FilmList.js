2bb77008d25cb6755f9c56d7e49f00316000fe69
Raquel Magnani <quel.magnani88@gmail.com>
2022-04-29 14:09:19 +0200

    creates generic card and replaces it on Film List

--- src/components/FilmList.js
+++ src/components/FilmList.js
@@ -1,24 +1,28 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import "../styles/filmList.scss";
-import CardFilm from "./CardFilm";
+import GenericCard from "./GenericCard";
 
 const FilmList = ({ films }) => {
-  console.log(films);
   return (
     <>
       <Container fluid>
+        <h1 className="d-flex justify-content-center">Star Wars Movies!</h1>
         <Row>
           {films.map((film) => {
             return (
-              <Col>
-                <CardFilm film={film} />
+              <Col lg={3} md={6} className="mt-3 d-flex justify-content-center">
+                <GenericCard
+                  name={film.title}
+                  resourceImage="films"
+                  resource="film"
+                  url={film.url}
+                ></GenericCard>
               </Col>
             );
           })}
         </Row>
       </Container>
-      ;
     </>
   );
 };
---
+++ src/components/GenericCard.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Card, CardGroup } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "../styles/cardFilm.scss";
+import { getIdFromUrl } from "../utils";
+
+const GenericCard = (props) => {
+  return (
+    <>
+      <CardGroup>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img
+            variant="top"
+            alt={`poster of ${props.name}`}
+            src={`https://starwars-visualguide.com/assets/img/${
+              props.resourceImage
+            }/${getIdFromUrl(props.url)}.jpg`}
+          />
+          <Card.Body>
+            <Link
+              className="d-flex justify-content-center link"
+              to={`/${props.resource}/${getIdFromUrl(props.url)}`}
+            >
+              {props.name}
+            </Link>
+          </Card.Body>
+        </Card>
+      </CardGroup>
+    </>
+  );
+};
+
+export default GenericCard;
