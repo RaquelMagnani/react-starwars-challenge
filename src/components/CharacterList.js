72ff2250f4785af604e89db170b47c47401c3d9a
Raquel Magnani <quel.magnani88@gmail.com>
2022-04-30 14:46:21 +0200

    fixes characters list and add is loading

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,26 +2,15 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getIdFromUrl } from "../utils";
 
-const CharactersList = ({ cast }) => {
-  const [characters, setCharacters] = useState([]);
-
-  useEffect(() => {
-    if (cast?.length > 0 && characters.length === 0) {
-      let castArr = [];
-      cast.forEach((element) => {
-        fetch(element)
-          .then((response) => response.json())
-          .then((data) => castArr.push(data));
-      });
-      setCharacters(castArr);
-    }
-  }, [cast]);
-
+const CharactersList = ({ characters }) => {
   return (
     <ul className="container-infos">
-      {characters.map((character) => (
-        <li key={`unique-${character.index}-`} style={{ textAlign: "left" }}>
-          <i class="bi bi-person-circle p-2"></i>
+      {characters?.map((character, index) => (
+        <li
+          key={`unique-${character.url}-${index}`}
+          style={{ textAlign: "left" }}
+        >
+          <i className="bi bi-person-circle p-2"></i>
           <Link className="link" to={`/people/${getIdFromUrl(character.url)}`}>
             {character.name}
           </Link>
--- src/components/Film.js
+++ src/components/Film.js
@@ -6,48 +6,115 @@ import CharactersList from "./CharacterList";
 const Film = () => {
   const [film, setFilm] = useState({});
   const { id } = useParams();
+  // verify when a promise started
+  const [isLoading, setIsLoading] = useState(false);
+  // verify when a film promise is finished
+  const [filmDataReceived, setfilmDataReceived] = useState(false);
+  // verify if happen some error
+  const [isError, setIsError] = useState(false);
+  // save all characters data
+  const [characters, setCharacters] = useState([]);
+
   useEffect(() => {
-    fetch(`https://swapi.dev/api/films/${id}/`)
-      .then((response) => response.json())
-      .then((data) => setFilm(data));
+    const fetchFilmData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+      try {
+        const result = await fetch(`https://swapi.dev/api/films/${id}/`);
+        const data = await result.json();
+
+        setfilmDataReceived(true);
+        setFilm(data);
+      } catch (error) {
+        console.log(error);
+        setIsError(true);
+      }
+      setIsLoading(false);
+    };
+
+    fetchFilmData();
   }, []);
+
+  useEffect(() => {
+    if (filmDataReceived && film && film.characters.length > 0) {
+      film?.characters?.forEach(async (element) => {
+        setIsError(false);
+        setIsLoading(true);
+        try {
+          const result = await fetch(element);
+          const data = await result.json();
+          setCharacters((prevState) => {
+            if (prevState.includes(data.name)) return prevState;
+            return [
+              ...prevState,
+              {
+                name: data.name,
+                url: data.url,
+              },
+            ];
+          });
+        } catch (error) {
+          setIsError(true);
+        }
+        setIsLoading(false);
+      });
+    }
+  }, [filmDataReceived]);
+
   return (
     <Container fluid>
-      <Row>
-        <Col className md={8}>
-          <Row>
-            <Col>
-              <div className="container-infos p-2 mb-2 ">
-                <h1>{film.title}</h1>
-                <p>
-                  {" "}
-                  <span>Director: </span>
-                  {film.director}
-                </p>
-                <p>
-                  <span>About: </span>
-                  {film.opening_crawl}
-                </p>
-              </div>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <div className="container-infos p-2">
-                <span>Characters: </span>
-
-                <CharactersList cast={film.characters} />
-              </div>
-            </Col>
-          </Row>
-        </Col>
-        <Col md={4}>
-          <img
-            alt={`poster of ${film.title}`}
-            src={`https://starwars-visualguide.com/assets/img/films/${id}.jpg`}
-          />
-        </Col>
-      </Row>
+      {isError && (
+        <Row>
+          <Col>
+            <p>Something went wrong ...</p>
+          </Col>
+        </Row>
+      )}
+      {isLoading ? (
+        <Row>
+          <Col>
+            <p>Loading.... ...</p>
+          </Col>
+        </Row>
+      ) : (
+        <Row>
+          <Col className md={8}>
+            <Row>
+              <Col>
+                <div className="container-infos p-2 mb-2 ">
+                  <h1>{film.title}</h1>
+                  <p>
+                    <span>Director: </span>
+                    {film.director}
+                  </p>
+                  <p>
+                    <span>About: </span>
+                    {film.opening_crawl}
+                  </p>
+                </div>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <div className="container-infos p-2">
+                  <span>Characters: </span>
+                  {isLoading ? (
+                    <span>Loading...</span>
+                  ) : (
+                    <CharactersList characters={characters} />
+                  )}
+                </div>
+              </Col>
+            </Row>
+          </Col>
+          <Col md={4}>
+            <img
+              alt={`poster of ${film.title}`}
+              src={`https://starwars-visualguide.com/assets/img/films/${id}.jpg`}
+            />
+          </Col>
+        </Row>
+      )}
     </Container>
   );
 };
